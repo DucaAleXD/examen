0c2e0646ded2b9293ae7c47d1159d671ea61acaa
DucaAlex1 <duca.alexandr@elev.cihcahul.md>
2024-06-24 17:01:23 +0300

    Adaugaerea model migrarea si Controlerr cu comanda php artisan make:Pahe -mcr

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -29,11 +29,118 @@
         </div>
     </div>
     </nav>
+  <header class="header">
+    <div class="container">
+      <h1>Platforma de promovare</h1>
+      <p>Centrul Republican de Învățământ din subordinea Ministerului Educației Publice</br> Platforma unică servește la popularizarea celor mai bune practici ale pedagogilor</p>
+      <a href="#" class="btn btn-primary">Înscrie-te</a>
+      <a href="#" class="btn btn-secondary">Autentificare</a>
+    </div>
+  </header>
+  
+ 
+    <div class="row">
+        <div class="column" >
+            <div class="marginSection">
+                <h2>We connect our customers with the best, and help them keep up-and stay open.</h2>
+                <div class="mt-5">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                </div>
+            </div>
+    </div>
+        <div class="column" >
+            <div class="relative">
+                <img src="https://images.pexels.com/photos/719396/pexels-photo-719396.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="">
+                <div class="absolute">
+                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQdXWh72DgpiId3gg_1YgyEtH7VY_huEZ3kuQ&s" alt="">
+                </div>
+            </div>
+    </div>
 
+    <div class="center">
+            <h2>Produse Accesibile</h2>
+    </div>
+
+    <div class="center">
+        <div class="row row-cols-1 row-cols-md-3 g-4">
+            <div class="col">
+                <div class="card">
+                <img src="..." class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card">
+                <img src="..." class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card">
+                <img src="..." class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+                </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card">
+                <img src="..." class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                </div>
+                </div>
+            </div>
+            </div>
+    </div>
+
+        <div class="center mt-5">
+            <h2>Produse CRUD</h2>
+        </div>
+        <div class="center">
+            <div class="row">
+            <div class="col-sm-6 mb-3 mb-sm-0">
+                <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">Special title treatment</h5>
+                    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+                </div>
+            </div>
+            <div class="col-sm-6">
+                <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">Special title treatment</h5>
+                    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+                </div>
+            </div>
+            </div>
+        </div>
 
     @yield('content')
-    <footer>
 
-    </footer>
+  <footer class="py-3 my-4">
+    <ul class="nav justify-content-center border-bottom pb-3 mb-3">
+      <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Home</a></li>
+      <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Features</a></li>
+      <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Pricing</a></li>
+      <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">FAQs</a></li>
+      <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">About</a></li>
+    </ul>
+    <p class="text-center text-body-secondary">© 2024 Company, Inc</p>
+  </footer>
 </body>
 </html>
